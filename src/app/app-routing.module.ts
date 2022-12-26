39af832948e9b5bb35864347545d6cdabe7a8ea4
DayanaLorente <lorenteericka@hotmail.com>
2022-12-26 14:32:46 -0500

    primer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { PromocionesComponent } from './promociones/promociones.component';
+import { RecuperacionComponent } from './recuperacion/recuperacion.component';
+import { RegistroComponent } from './registro/registro.component';
+import { VisionComponent } from './vision/vision.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'registro', component: RegistroComponent},
+  {path: 'menu', component: MenuComponent},
+  {path: 'recuperacion', component: RecuperacionComponent},
+  {path: 'vision', component: VisionComponent},
+  {path: 'promociones', component: PromocionesComponent},
+  {path: 'galeria', component: PromocionesComponent},
+  {path: '**', redirectTo: 'login',pathMatch:'full'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+import { VisionComponent } from './vision/vision.component';
+import { MenuComponent } from './menu/menu.component';
+import { PromocionesComponent } from './promociones/promociones.component';
+import { RecuperacionComponent } from './recuperacion/recuperacion.component';
+import { GaleriaComponent } from './galeria/galeria.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    VisionComponent,
+    MenuComponent,
+    PromocionesComponent,
+    RecuperacionComponent,
+    GaleriaComponent
   ],
   imports: [
     BrowserModule,
